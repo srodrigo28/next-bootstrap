d162ce55d64d5b7234bfc8d4d9b7194e5bdf9d5c
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-01-08 16:27:51 -0300

    updated-crud-and-filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   const [id, setId] = useState("");
   const [nome, setNome] = useState("");
   const [desc, setDesc] = useState("");
+  const [search, setSearch] = useState(""); // Novo estado para busca
   
   const [data, setData] = useState([]);
   const [classInserir, setClassInserir] = useState("");
@@ -29,7 +30,7 @@ export default function Home() {
       .then(res => setData(res.data))
   }, [data, setData]);
 
-  // Inserir
+  // Inserir produto
   const Inserir = () => {
     axios.post(url, {
       nome,
@@ -65,10 +66,7 @@ export default function Home() {
 
   /* Remover */
   const Remover = (id: string, nome: string) => {
-    // alert("Deletando no id: " + id)
-
     const confirm = window.confirm('Deseja realmente apagar  ? ' + nome )
-
     if(confirm){  
       setId(id)
 
@@ -85,13 +83,10 @@ export default function Home() {
         transition: Bounce,
       });
     }
-
   }
 
   /* Carregar campos */
   const Carregar = (id: string, nome: string, desc: string) => {
-    // alert("Id: " + id + " Nome: " + nome + " Descrição: " + desc);
-
     setId(id);
     setNome(nome);
     setDesc(desc);
@@ -100,93 +95,106 @@ export default function Home() {
     setClassInserir("sumir");
   }
 
-  /* Carregar campos */
+  /* Editar produto */
   const Editar = async (event: FormEvent) => {
     event.preventDefault();
 
-    // console.log("Nome: " + nome + " Desc: " + desc);
+    await axios.put(url + "/" + id, { nome, desc })
+      .then(() => {
+        toast.success('A Categoria foi Editada com Sucesso!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
 
-    await axios.put(url + "/" + id, {
-      nome,
-      desc
-    })
-    .then(() => {
-      toast.success('A Categoria foi Editada com Sucesso!', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Bounce,
-      });
-
-      setId("")
-      setNome("")
-      setDesc("")
-
-      setClassEditar("sumir")
-      setClassInserir("")
-    })
+        setId("")
+        setNome("")
+        setDesc("")
+        setClassEditar("sumir")
+        setClassInserir("")
+      })
   }
+
+  /* Filtrar dados com base na busca */
+  const filteredData = data.filter((item: ICategoria) =>
+    item.nome.toLowerCase().includes(search.toLowerCase())
+  );
 
   /* Mostrar na tela */
   return (
     <div className="container mt-4">      
+      
       <h2>Cadastro de Categorias</h2>
 
       <form className="mt-5 mb-4">
-        <div className="row mb-3">
-          <div className="col">
-            <input
-              type="text"
-              placeholder='Nome da Categoria'
-              className='form-control'
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
+          {/* Campo de busca */}
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                placeholder="Buscar por Nome"
+                className="form-control"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </div>
           </div>
-          
-          <div className="col">
-            <input
-              type="text"
-              placeholder='Descrição da Categoria'
-              className='form-control'
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-            />
+          {/* Campo de nome da categoria */}
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                placeholder="Nome da Categoria"
+                className="form-control"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </div>
+            {/* Campo de descrição */}
+            <div className="col">
+              <input
+                type="text"
+                placeholder="Descrição da Categoria"
+                className="form-control"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="d-flex items-start gap-2">
-          <button className={`btn btn-success rounded-md ${classInserir}`} onClick={Cadastrar}>Inserir</button>
-          <button className={`btn btn-warning rounded-md ${classEditar}`} onClick={Editar}>Alterar</button>
-        </div>
+          {/* Botões e controle */}
+          <div className="d-flex items-start gap-2">
+            <button className={`btn btn-success rounded-md ${classInserir}`} onClick={Cadastrar}>Inserir</button>
+            <button className={`btn btn-warning rounded-md ${classEditar}`} onClick={Editar}>Alterar</button>
+          </div>
       </form>
 
       <h2 className='mb-4'>Lista de Categorias</h2>
+      
       <table className="table table">
         <thead>
           <tr>
             <td>ID</td>
             <td>Categoria</td>
             <td>Descrição</td>
-            <td>Ações</td>
+            <td className='text-center'>Ações</td>
           </tr>
         </thead>
         <tbody>
-          {data.map((item: ICategoria) => (
+          {filteredData.map((item: ICategoria) => (
             <tr key={item.id}>
               <td>{item.id}</td>
               <td>{item.nome}</td>
               <td className='mx-2'>{item.desc}</td>
-              <td className="mx-2">
+              <td className="mx-2 text-center">
                 <button className="btn btn-warning mx-2" onClick={() => Carregar(item.id, item.nome, item.desc)}>
                   <FaPen  />
                 </button>
-
                 <button className="btn btn-danger" onClick={() => Remover(item.id, item.nome)}>
                   <FaTrash />
                 </button>
